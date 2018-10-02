742464fa9b21912c99d2efb837cf3f2b4ed7dcf4
Zdenek Kubala <kubala@ygg.cz>
2018-10-02 14:24:48 +0200

    Update piwik.js
    fix site id for piwik

--- _includes/js/piwik.js
+++ _includes/js/piwik.js
@@ -5,7 +5,7 @@ _paq.push(['enableLinkTracking']);
 (function() {
   var u="//piwik.pirati.cz/";
   _paq.push(['setTrackerUrl', u+'piwik.php']);
-  _paq.push(['setSiteId', '3']);
+  _paq.push(['setSiteId', '11']);
   var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
   g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
 })();
